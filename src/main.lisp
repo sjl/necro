65880544b3887d0036f8fcca73cc245fb46e93be
Steve Losh <steve@stevelosh.com>
2017-10-31 19:43:50 -0400

    Add actions and labels

--- src/main.lisp
+++ src/main.lisp
@@ -5,7 +5,16 @@
 (defvar *width* 1)
 (defvar *height* 1)
 
-(defvar *pages-read* nil)
+(defvar *pages* nil)
+(defvar *unlocked* nil)
+
+;;;; Utilities ----------------------------------------------------------------
+(defun required ()
+  (error "Required."))
+
+(defun p (x y format-string &rest args)
+  (charms:write-string-at-point t (apply #'format nil format-string args) x y))
+
 
 ;;;; Input --------------------------------------------------------------------
 (defun update-window-size ()
@@ -13,32 +22,56 @@
         (charms:window-dimensions t)))
 
 
-;;;; Drawing ------------------------------------------------------------------
-(defun p (x y format-string &rest args)
-  (charms:write-string-at-point t (apply #'format nil format-string args) x y))
+;;;; Components ---------------------------------------------------------------
+(defclass* component ()
+  ((draw :type (function (integer integer)) :initform (required))
+   (key :type (or null character) :initform nil)
+   (action :type (or null function) :initform nil)))
 
-(defun draw-pages-read ()
-  (p 0 0 "Pages read: ~D" *pages-read*))
+;;;; Pages --------------------------------------------------------------------
+(defun make-pages-read ()
+  (make-instance 'component
+    :draw (lambda (x y)
+            (p x y "Pages read: ~D" *pages*))))
+
+(defun make-read-page! ()
+  (make-instance 'component
+    :draw (lambda (x y)
+            (p x y "[ (R)ead page ]"))
+    :key #\r
+    :action (lambda ()
+              (incf *pages*))))
+
+
+;;;; Drawing ------------------------------------------------------------------
+(defun draw-component (c x y)
+  (funcall (component-draw c) x y))
 
 (defun draw-help ()
   (p 0 (1- *height*) "[Q]uit"))
 
 (defun draw-screen ()
   (charms:clear-window t)
-  (draw-pages-read)
+  (iterate (for component :in *unlocked*)
+           (for y :from 0)
+           (draw-component component 0 y))
   (draw-help)
   (charms:update))
 
 
 ;;;; Main ---------------------------------------------------------------------
 (defun initialize ()
-  (setf *pages-read* 0
-        *running* t))
+  (setf *pages* 0
+        *running* t
+        *unlocked* (list (make-pages-read)
+                         (make-read-page!))))
 
 (defun handle-event (event)
   (case event
     (#\Q (setf *running* nil))
-    (:resize (update-window-size))))
+    (:resize (update-window-size))
+    (t (when-let ((component (find event *unlocked* :key #'component-key)))
+         (funcall (component-action component))))))
 
 
 (defun handle-events ()
