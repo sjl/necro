543296f54e5a1504401025faa300c0fdd765c99c
Steve Losh <steve@stevelosh.com>
2017-10-31 19:11:54 -0400

    Add a basic game loop

--- src/main.lisp
+++ src/main.lisp
@@ -1,31 +1,68 @@
 (in-package :necro)
 
 ;;;; State --------------------------------------------------------------------
+(defvar *running* nil)
 (defvar *width* 1)
 (defvar *height* 1)
+
+(defvar *pages-read* nil)
 
 ;;;; Input --------------------------------------------------------------------
 (defun update-window-size ()
   (setf (values *width* *height*)
         (charms:window-dimensions t)))
 
+
 ;;;; Drawing ------------------------------------------------------------------
+(defun p (x y format-string &rest args)
+  (charms:write-string-at-point t (apply #'format nil format-string args) x y))
+
 (defun redraw ()
   (charms:update))
 
+(defun draw-pages-read ()
+  (p 0 0 "Pages read: ~D" *pages-read*))
+
+(defun draw-screen ()
+  (draw-pages-read)
+  (redraw))
+
 
 ;;;; Main ---------------------------------------------------------------------
+(defun initialize ()
+  (setf *pages-read* 0
+        *running* t))
+
+(defun handle-event (event)
+  (case event
+    (#\Q (setf *running* nil))))
+
+
+(defun handle-events ()
+  (iterate
+    (for event = (charms:get-char t :ignore-error t))
+    (while event)
+    (handle-event event)))
+
+
+(defun game-loop ()
+  (iterate (while *running*)
+           (draw-screen)
+           (handle-events)
+           (sleep 1/30)))
+
 (defun run ()
+  (initialize)
   (charms:with-curses ()
     (charms:disable-echoing)
     (charms:enable-raw-input :interpret-control-characters t)
+    (charms:enable-non-blocking-mode t)
     (charms:enable-extra-keys t)
     (charms/ll:start-color)
     (charms/ll:curs-set 0)
     (charms:clear-window t)
     (update-window-size)
-    (charms:write-string-at-point t "Necro!" 0 0)
-    (charms:get-char t)))
+    (game-loop)))
 
 
 ;;;; Entry --------------------------------------------------------------------
